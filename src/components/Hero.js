31a0677727e4fe8ef99c23550cf159efea38c8ad
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-12 22:40:38 +0200

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,5 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
+const Hero = () => {
+  return <div>chwdp</div>;
+};
 
-export default function Hero() {
-  return (
-    <Box
-      sx={{
-        width: 500,
-        height: 300,
-        backgroundColor: "primary.dark",
-        "&:hover": {
-          backgroundColor: "primary.main",
-          opacity: [0.9, 0.8, 0.7],
-        },
-      }}
-    >
-      <h1>Welcome to QuizME!</h1>
-    </Box>
-  );
-}
+export default Hero;
