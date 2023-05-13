af2eb43f5ab1f75fd5e7de484a8d6a43c6bf2f96
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-13 08:46:53 +0200

    MUI instalation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,22 @@
+import styled from "@emotion/styled";
+import { Box, Button } from "@mui/material";
+
 const Hero = () => {
-  return <div>chwdp</div>;
+  return (
+    <StyledBox>
+      <h1>Welcome to QuizME!</h1>
+      <p>
+        Answer the following questions to learn which mythical creature suit you
+        the best!
+      </p>
+      <Button variant='contained'>Contained</Button>
+    </StyledBox>
+  );
 };
 
 export default Hero;
+
+const StyledBox = styled(Box)`
+  max-width: 700px;
+  background-color: azure;
+`;
