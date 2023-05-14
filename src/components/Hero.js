4cc3704630a27f22ca0be2a7c05b0feca575b275
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-14 08:35:05 +0200

    styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,20 @@
 import styled from "@emotion/styled";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 const Hero = () => {
   return (
-    <StyledBox>
-      <h1>Welcome to QuizME!</h1>
-      <p>
-        Answer the following questions to learn which mythical creature suit you
-        the best!
-      </p>
-      <Button variant='contained'>Contained</Button>
-    </StyledBox>
+    <StyledBox2>
+      <StyledBox>
+        <Typography variant='h2' color='primary'>
+          Welcome to QuizME!
+        </Typography>
+        <Typography variant='subtitle1'>
+          Answer the following questions to learn which mythical creature suit
+          you the best!
+        </Typography>
+      </StyledBox>
+      <Button variant='contained'>Let's get started!</Button>
+    </StyledBox2>
   );
 };
 
@@ -18,5 +22,19 @@ export default Hero;
 
 const StyledBox = styled(Box)`
   max-width: 700px;
+  height: 200px;
   background-color: azure;
+  padding: 1rem;
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const StyledBox2 = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
