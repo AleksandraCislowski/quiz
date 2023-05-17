c933d140e7038f0d4871dfd678a8f93127fec53e
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-17 23:40:38 +0200

    result logic

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,10 +3,19 @@ const Result = (props) => {
     const biggestValue = [props.a, props.b, props.c, props.d].sort(
       (a, b) => b - a
     )[0];
-    if (props.a === biggestValue) return "A";
-    if (props.b === biggestValue) return "B";
-    if (props.c === biggestValue) return "C";
-    if (props.d === biggestValue) return "D";
+    let winners = [];
+    if (props.a === biggestValue) winners = [...winners, "A"];
+    if (props.b === biggestValue) winners = [...winners, "B"];
+    if (props.c === biggestValue) winners = [...winners, "C"];
+    if (props.d === biggestValue) winners = [...winners, "D"];
+    if (winners.length === 1) {
+      if (winners[0] === "A") return "1";
+      if (winners[0] === "B") return "2";
+      if (winners[0] === "C") return "3";
+      if (winners[0] === "D") return "4";
+    } else {
+      return "5";
+    }
   };
 
   return (
