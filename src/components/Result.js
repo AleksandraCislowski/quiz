c3fb3f1bdc6e73e02f2f7bc1c203dc28768a6795
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-18 10:45:51 +0200

    styling result nr 1

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,3 +1,8 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import unicorn from "./pictures/unicorn.jpg";
+import { Typography } from "@mui/material";
+
 const Result = (props) => {
   const displayResult = () => {
     const biggestValue = [props.a, props.b, props.c, props.d].sort(
@@ -9,7 +14,23 @@ const Result = (props) => {
     if (props.c === biggestValue) winners = [...winners, "C"];
     if (props.d === biggestValue) winners = [...winners, "D"];
     if (winners.length === 1) {
-      if (winners[0] === "A") return "1";
+      if (winners[0] === "A")
+        return (
+          <StyledBox sx={{ backgroundImage: `url(${unicorn})` }}>
+            <h2>UNICORN</h2>
+            <Typography variant='subtitle1' color='white'>
+              Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+              przemyśle poligraficznym. Został po raz pierwszy użyty w XV w.
+              przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+              Pięć wieków później zaczął być używany przemyśle elektronicznym,
+              pozostając praktycznie niezmienionym. Spopularyzował się w latach
+              60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających
+              fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje
+              Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na
+              komputerach osobistych, jak Aldus PageMaker
+            </Typography>
+          </StyledBox>
+        );
       if (winners[0] === "B") return "2";
       if (winners[0] === "C") return "3";
       if (winners[0] === "D") return "4";
@@ -27,3 +48,19 @@ const Result = (props) => {
 };
 
 export default Result;
+
+const StyledBox = styled(Box)`
+  max-width: 700px;
+  padding: 1rem;
+  margin: 3rem;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+`;
