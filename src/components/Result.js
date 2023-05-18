6feec6a6de65902c010f4d6b726b5d703afaf737
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-18 10:58:19 +0200

    styling all the results

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,11 @@
 import styled from "@emotion/styled";
 import { Box } from "@mui/system";
+import { Button, Typography } from "@mui/material";
 import unicorn from "./pictures/unicorn.jpg";
-import { Typography } from "@mui/material";
+import siren from "./pictures/siren.jpg";
+import dragon from "./pictures/dragon.jpg";
+import centaur from "./pictures/centaur.jpg";
+import pegasus from "./pictures/pegasus.jpg";
 
 const Result = (props) => {
   const displayResult = () => {
@@ -31,19 +35,84 @@ const Result = (props) => {
             </Typography>
           </StyledBox>
         );
-      if (winners[0] === "B") return "2";
-      if (winners[0] === "C") return "3";
-      if (winners[0] === "D") return "4";
+      if (winners[0] === "B")
+        return (
+          <StyledBox sx={{ backgroundImage: `url(${siren})` }}>
+            <h2>SIREN</h2>
+            <Typography variant='subtitle1' color='white'>
+              Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+              przemyśle poligraficznym. Został po raz pierwszy użyty w XV w.
+              przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+              Pięć wieków później zaczął być używany przemyśle elektronicznym,
+              pozostając praktycznie niezmienionym. Spopularyzował się w latach
+              60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających
+              fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje
+              Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na
+              komputerach osobistych, jak Aldus PageMaker
+            </Typography>
+          </StyledBox>
+        );
+      if (winners[0] === "C")
+        return (
+          <StyledBox sx={{ backgroundImage: `url(${dragon})` }}>
+            <h2>DRAGON</h2>
+            <Typography variant='subtitle1' color='white'>
+              Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+              przemyśle poligraficznym. Został po raz pierwszy użyty w XV w.
+              przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+              Pięć wieków później zaczął być używany przemyśle elektronicznym,
+              pozostając praktycznie niezmienionym. Spopularyzował się w latach
+              60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających
+              fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje
+              Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na
+              komputerach osobistych, jak Aldus PageMaker
+            </Typography>
+          </StyledBox>
+        );
+      if (winners[0] === "D")
+        return (
+          <StyledBox sx={{ backgroundImage: `url(${centaur})` }}>
+            <h2>CENTAUR</h2>
+            <Typography variant='subtitle1' color='white'>
+              Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+              przemyśle poligraficznym. Został po raz pierwszy użyty w XV w.
+              przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+              Pięć wieków później zaczął być używany przemyśle elektronicznym,
+              pozostając praktycznie niezmienionym. Spopularyzował się w latach
+              60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających
+              fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje
+              Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na
+              komputerach osobistych, jak Aldus PageMaker
+            </Typography>
+          </StyledBox>
+        );
     } else {
-      return "5";
+      return (
+        <StyledBox sx={{ backgroundImage: `url(${pegasus})` }}>
+          <h2>PEGASUS</h2>
+          <Typography variant='subtitle1' color='white'>
+            Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+            przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez
+            nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć
+            wieków później zaczął być używany przemyśle elektronicznym,
+            pozostając praktycznie niezmienionym. Spopularyzował się w latach
+            60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających
+            fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem
+            Ipsum oprogramowaniem przeznaczonym do realizacji druków na
+            komputerach osobistych, jak Aldus PageMaker
+          </Typography>
+        </StyledBox>
+      );
     }
   };
 
   return (
-    <div>
+    <StyledBox2>
       <div>{displayResult()}</div>
-      <button onClick={props.resetQuiz}>Retake the quiz!</button>
-    </div>
+      <Button variant='contained' onClick={props.resetQuiz}>
+        Retake the quiz!
+      </Button>
+    </StyledBox2>
   );
 };
 
@@ -63,4 +132,11 @@ const StyledBox = styled(Box)`
   background-size: cover;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
+`;
+
+const StyledBox2 = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
